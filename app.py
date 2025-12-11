e3c0456af351465e369c674a019219a6edc75270
jayanta8509 <jayantameslova@gmail.com>
2025-12-11 16:18:51 +0530

    port chnage

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=8021,
+        port=8001,
         reload=True,
         log_level="info"
     )
